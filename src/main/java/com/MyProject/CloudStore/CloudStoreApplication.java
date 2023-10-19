9a56b2353160094e8bdb4703531ee465f26e58c1
nikitagagua <2gagua121@gmail.com>
2023-10-19 21:33:37 +0300

    intlz+

--- src/main/java/com/MyProject/CloudStore/CloudStoreApplication.java
+++ src/main/java/com/MyProject/CloudStore/CloudStoreApplication.java
@@ -9,5 +9,7 @@ public class CloudStoreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CloudStoreApplication.class, args);
 	}
-
+	57.32 -> 57.16 = 0.16
+	57.16 -> 57.05 = 0.11
+	57.05 -> 56.98 = 0.7
 }
